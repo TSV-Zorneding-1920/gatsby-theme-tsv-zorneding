815ac523e77a22bc87be3a287c2e0c15f07606a7
Christian Fritsch <chr.fritsch@gmx.net>
2019-11-14 19:59:00 +0100

    Modify order

--- cms/cms.js
+++ cms/cms.js
@@ -3,10 +3,6 @@ import CMS from "netlify-cms-app";
 import BlogPostPreview from "./preview-templates/BlogPostPreview";
 import styles from "!css-loader!sass-loader!./../sass/main.scss";
 
-CMS.registerPreviewStyle(styles.toString(), { raw: true });
-
-CMS.registerPreviewTemplate("blog", BlogPostPreview);
-
 CMS.init({
   config: {
     backend: {
@@ -298,3 +294,7 @@ CMS.init({
     ]
   }
 });
+
+CMS.registerPreviewStyle(styles.toString(), { raw: true });
+
+CMS.registerPreviewTemplate("blog", BlogPostPreview);
