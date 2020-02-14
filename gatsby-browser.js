15d7b96ee2d2c9c9b13538aa2e9c4963a293e812
Christian Fritsch <chr.fritsch@gmx.net>
2020-02-14 11:04:36 +0100

    feat: add reload msg

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,3 +4,13 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 import "./../gatsby-theme-tsv-zorneding/sass/main.scss";
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  );
+  if (answer === true) {
+    window.location.reload();
+  }
+};
